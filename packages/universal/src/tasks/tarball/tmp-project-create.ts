f7d5c233db8b6636ba5d9b9ff2e277760b6b1588
Rafa Mel <melrcm@gmx.net>
2024-07-09 15:17:04 +0200

    feat(packages/universal): handle monorepo workspaces when recreating a project for tarball

--- packages/universal/src/tasks/tarball/tmp-project-create.ts
+++ packages/universal/src/tasks/tarball/tmp-project-create.ts
@@ -1,38 +1,82 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
-import { type Task, copy, create, series, tmp } from 'kpo';
+import { glob } from 'glob';
+import { type Task, copy, create, isCancelled, series, tmp } from 'kpo';
 
-// TODO: deal with monorepos
+import { getMonorepoRootDir, getPackageRootDir } from '@riseup/utils';
+
 export function tmpProjectCreate(
   callback: (directory: string) => Task
 ): Task.Async {
-  return tmp(null, ({ directory }) => {
-    return series(
-      // Copy project on temp directory
-      copy('./!(node_modules)', directory, {
-        glob: true,
-        single: false,
-        strict: true,
-        exists: 'error'
-      }),
-      // Link node_modules
-      create(async (ctx) => {
-        const nodeModulesPath = path.resolve(ctx.cwd, 'node_modules');
-        const nodeModulesDest = path.resolve(directory, 'node_modules');
-        const nodeModulesExist = await fs.promises
-          .access(nodeModulesPath, fs.constants.F_OK)
-          .then(
-            () => true,
-            () => false
-          );
+  return create(async (ctx) => {
+    const packageRoot = getPackageRootDir(ctx.cwd);
+    const projectRoot = getMonorepoRootDir(ctx.cwd) || packageRoot;
+    if (!packageRoot || !projectRoot) {
+      throw new Error(`Could not locate project root directory: ${ctx.cwd}`);
+    }
+    if (path.relative(ctx.cwd, packageRoot)) {
+      throw new Error(`Could not locate package at: ${ctx.cwd}`);
+    }
 
-        if (nodeModulesExist) {
-          await fs.promises.symlink(nodeModulesPath, nodeModulesDest, 'dir');
-        }
-      }),
-      // Run callback's Task
-      create(() => callback(directory))
-    );
+    const projectFiles = await glob('**/*', {
+      cwd: projectRoot,
+      absolute: true,
+      nodir: true,
+      ignore: {
+        childrenIgnored: (location) => location.name === 'node_modules'
+      }
+    });
+
+    return tmp(null, ({ directory }) => {
+      return series(
+        // Copy project on temp directory
+        copy(projectFiles, directory, {
+          glob: true,
+          single: false,
+          strict: true,
+          exists: 'error',
+          from: projectRoot
+        }),
+        // Link node_modules
+        create(async (ctx) => {
+          const locations = await glob('**/node_modules', {
+            absolute: false,
+            cwd: projectRoot,
+            ignore: {
+              childrenIgnored: (location) => location.name === 'node_modules'
+            }
+          });
+          if (isCancelled(ctx)) return;
+
+          for (const location of locations) {
+            const nodeModulesPath = path.resolve(projectRoot, location);
+            const nodeModulesDest = path.resolve(directory, location);
+            const nodeModulesExist = await fs.promises
+              .access(nodeModulesPath, fs.constants.F_OK)
+              .then(
+                () => true,
+                () => false
+              );
+            if (isCancelled(ctx)) return;
+
+            if (nodeModulesExist) {
+              await fs.promises.symlink(
+                nodeModulesPath,
+                nodeModulesDest,
+                'dir'
+              );
+              if (isCancelled(ctx)) return;
+            }
+          }
+        }),
+        // Run callback's Task
+        create(() => {
+          return callback(
+            path.join(directory, path.relative(projectRoot, packageRoot))
+          );
+        })
+      );
+    });
   });
 }
