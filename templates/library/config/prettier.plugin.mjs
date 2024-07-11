17b3457464dd7040caaaaf37d79d4cc5f7e4de00
Rafa Mel <melrcm@gmx.net>
2024-07-11 17:57:57 +0200

    chore(templates): fix prettier configuration

--- templates/library/config/prettier.plugin.mjs
+++ templates/library/config/prettier.plugin.mjs
@@ -1,4 +1,4 @@
 export default {
-  parsers: { ignores: { astFormat: 'ignores', parse: (text) => ({ text }) } },
-  printers: { ignores: { print: (path) => path.getValue().text } }
+  parsers: { ignores: { astFormat: 'ignores', parse: (value) => value } },
+  printers: { ignores: { print: (path) => path.getValue() } }
 };
